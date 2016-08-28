ca7799170048f863e7d92cd6af47993a68675906
fengyanyan <Fyy@showgold.cn>
2016-08-28 10:47:46 +0800

    aa

---
+++ demo1/index.ios副本 2.js
@@ -0,0 +1,14 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ * 这个是Demo1，主要阐述的是
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry
+} from 'react-native';
+
+import App from './app/App.js';
+AppRegistry.registerComponent('wabg', () => App);
